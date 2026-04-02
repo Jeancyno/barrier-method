890744fae822ce3ec77e7e088bfd9063c2b46880
Jovialngandu <jovialngandu2@gmail.com>
2026-04-02 22:53:22 +0100

    [UPDATE] Supression des fichiers ne contribuant pas au programme final

--- README.md
+++ README.md
@@ -144,7 +144,7 @@ pip install -r requirements.txt
 ## ▶️ Exécution
 
 ```bash
-python main.py
+python app.py
 ```
 
 
--- src/problem/objective.py
+++
@@ -1,9 +0,0 @@
-def grad_f(x):
-    return [3, 2]
-
-
-def hess_f(x):
-    return [
-        [0, 0],
-        [0, 0]
-    ]
